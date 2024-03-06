4fb1858c09444ec696e5f41194d0aca31cf6d702
Moch Rayhan A <mochrayhan2910@gmail.com>
2024-03-06 14:05:04 +0700

    [FEATURE] menambahkan fitur report harian

---
+++ model/dto/transactionDto/transactionDto.go
@@ -0,0 +1,13 @@
+package transactionDto
+
+type Transactions struct {
+	Transaction_id string
+	Employee_id    string
+	Room_id        string
+	StartDate      string
+	EndDate        string
+	Description    string
+	Status         string
+	Created_at     string
+	Updated_at     string
+}
--- router/initRouter.go
+++ router/initRouter.go
@@ -4,6 +4,9 @@ import (
 	"BookingRoom/src/employees/employeeDelivery"
 	"BookingRoom/src/employees/employeeRepository"
 	"BookingRoom/src/employees/employeeUsecase"
+	"BookingRoom/src/transactions/transactionDelivery"
+	"BookingRoom/src/transactions/transactionRepository"
+	"BookingRoom/src/transactions/transactionUsecase"
 	"database/sql"
 
 	"github.com/gin-gonic/gin"
@@ -12,10 +15,13 @@ import (
 func InitRouter(v1Group *gin.RouterGroup, db *sql.DB) {
 	// repository
 	employeeRepo := employeeRepository.NewEmployeeRepository(db)
+	transactionRepo := transactionRepository.NewTransactionRepo(db)
 
 	// usecase
 	employeeUC := employeeUsecase.NewEmployeeUsecase(employeeRepo)
+	transactionUC := transactionUsecase.NewTransactionUsecase(transactionRepo)
 
 	// delivery
 	employeeDelivery.NewEmployeeDelivery(v1Group, employeeUC)
+	transactionDelivery.NewTransactionDelivery(v1Group, transactionUC)
 }
